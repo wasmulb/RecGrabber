87edbabc458ad5d2eef52acf04de76388bc51719
Conner Laursen <conner@Conners-MacBook-Air.local>
2022-12-01 21:32:19 -0800

    added fetch to test the api

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+fetch('https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0',{
+    
+    headers: {
+        apikey: "eed7d646-5122-4f56-acec-03d9654afba8"
+    }
+})
+  .then(function (response) {
+    console.log(response);
+  })
+  .then(function (data) {
+    console.log(data);
+  });
